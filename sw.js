72ca14057c5e2335003fa31e5fc632f035a1a70c
anshika gupta <anshika1109@gmail.com>
2026-06-25 14:28:35 -0700

    Initial commit: Spark Homes Repair Estimator PWA

---
+++ sw.js
@@ -0,0 +1,12 @@
+// Self-destructing service worker - clears all caches and unregisters
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
